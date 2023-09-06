d952519eb89a182de774e4c42ef0ee3cf01be470
Kiss Ákos <bertram.webdesign@gmail.com>
2023-09-06 13:08:42 +0200

    adding to the correct list works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoapp';
+
+  dataInput: string ='';
+  forTodayCheck: boolean = false;
+  forTomorrowCheck: boolean = false;
+  todayList: string[] = [];
+  tomorrowList: string[] = [];
+
+  load(){
+    if(this.forTodayCheck){
+      this.todayList.push(this.dataInput);
+    }
+
+    if(this.forTomorrowCheck){
+      this.tomorrowList.push(this.dataInput);
+    }
+  }
 }
+
