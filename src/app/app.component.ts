80a9e7d51a9669d8eed44269f334e49c885e181c
Kiss Ákos <bertram.webdesign@gmail.com>
2023-09-06 13:33:45 +0200

    delete function works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,44 @@ export class AppComponent {
   dataInput: string ='';
   forTodayCheck: boolean = false;
   forTomorrowCheck: boolean = false;
+  todayListCheckbox: boolean [] = [];
+  tomorrowListCheckbox: boolean [] = [];
   todayList: string[] = [];
   tomorrowList: string[] = [];
 
   load(){
     if(this.forTodayCheck){
       this.todayList.push(this.dataInput);
+      this.todayListCheckbox.push(false);
     }
 
     if(this.forTomorrowCheck){
       this.tomorrowList.push(this.dataInput);
+      this.tomorrowListCheckbox.push(false);
+    }
+  }
+
+  moveToday(){
+
+  }
+
+  moveTomorrow(){
+    
+  }
+
+  moveOut(){
+    for( let i= this.todayListCheckbox.length -1; i>= 0; i--){
+      if(this.todayListCheckbox[i]){
+        this.todayList.splice(i, 1);
+        this.todayListCheckbox.splice(i, 1);
+      }
+    }
+
+    for( let i= this.tomorrowListCheckbox.length -1; i>= 0; i--){
+      if(this.tomorrowListCheckbox[i]){
+        this.tomorrowList.splice(i, 1);
+        this.tomorrowListCheckbox.splice(i, 1);
+      }
     }
   }
 }
